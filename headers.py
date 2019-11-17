2cd23fbaebbae066e9da2a0b9e8e0e4bcced29af
haski <perepelkin2601@gmail.com>
2019-11-17 15:57:19 +0500

    OptionalHeader and DataDir

---
+++ headers.py
@@ -0,0 +1,69 @@
+class MSDOS:
+    def __init__(self, bytes):
+        self.magic = bytes[0:2].decode('utf-8')
+        self.page_count = bytes[2:4]
+        self.reloc_count = bytes[4:6]
+        self.header_size = bytes[6:8]
+        self.min_alloc = bytes[8:10]
+        self.max_alloc = bytes[10:12]
+        self.init_ss = bytes[12:14]
+        self.init_sp = bytes[14:16]
+        self.check_sum = bytes[16:18]
+        self.init_ip = bytes[18:20]
+        self.init_cs = bytes[20:22]
+        self.reloc_addr = bytes[22:24]
+        self.overlay_count = bytes[24:26]
+        self.oem_identifier = bytes[36:38]
+        self.oem_info = bytes[38:40]
+        self.pe_header_addr = int.from_bytes(bytes[60:64], "little")
+
+
+class PE:
+    def __init__(self, bytes):        
+        self.magic = bytes[0:4]
+        self.cpu_pype = bytes[4:6]
+        self.section_count = bytes[6:8]
+        self.data_time = bytes[8:12]
+        self.symbol_table_addr = bytes[12:16]
+        self.symbol_table_size = bytes[16:20]
+        self.optional_header_size = bytes[20:22]
+        self.flags = bytes[22:24]
+         
+
+class OptionalHeader:
+    def __init__(self, bytes):
+        self.magic = bytes[0:2]
+        self.major_link_ver = bytes[2:3]
+        self.minor_link_ver = bytes[3:4]
+        self.code_size = bytes[4:8]
+        self.init_data_size = bytes[8:12]
+        self.un_init_data_size = bytes[12:16]
+        self.entry_point_addr = bytes[16:20]
+        self.code_base = bytes[20:24]
+        self.data_base = bytes[24:28]
+        self.image_base = bytes[28:32]
+        self.section_align = bytes[32:36]
+        self.file_align = bytes[36:40]
+        self.major_os_ver = bytes[40:42]
+        self.minor_os_ver = bytes[42:44]
+        self.major_imaeg_ver = bytes[44:46]
+        self.monor_image_ver = bytes[46:48]
+        self.major_sub_sys_ver = bytes[48:50]
+        self.minor_sub_sys_ver = bytes[50:52]
+        self.image_size = bytes[56:60]
+        self.header_size = bytes[60:64]
+        self.check_sum = bytes[64:68]
+        self.sub_system = bytes[68:70]
+        self.dll_flags = bytes[70:72]
+        self.stack_reverse_size = bytes[72:76]
+        self.stack_commit_size = bytes[76:80]
+        self.heap_reverse_size = bytes[80:84]
+        self.heap_commit_size = bytes[84:88]
+        self.loader_flags = bytes[88:92]
+        self.data_dir_size = bytes[92:96]
+
+
+class DataDir:
+    def __init__(self, bytes):
+        self.virtual_address = bytes[0:4]
+        self.size = bytes[4:8]
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import headers
+
+
 if __name__ == "__main__":
-    pass
+    with open("sample.exe", "rb") as f:
+        b = f.read(64)
+        head1 = headers.MSDOS(b)
+        f.seek(head1.pe_header_addr)
+        b = f.read(120)
+        print(len(b))
+        head2 = headers.PE(b[:24])
+        head3 = headers.OptionalHeader(b[24:])
+        b = f.read(128)
+        print(len(b))
+        head4 = []
+        for i in range(16):
+            head4.append(headers.DataDir(b[4*i:4*i+8]))
